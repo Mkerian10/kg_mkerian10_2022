3e02f4a4e9b81b3285749055551182fb3cf5044d
Matthew <mkerian10@gmail.com>
2021-04-26 18:34:59 -0500

    Added implementation for num_phonetics#digitToWord and #numToPhonetics.
    Test implementation was also added for both functions.

--- num_phonetics/num_phonetics.js
+++ num_phonetics/num_phonetics.js
@@ -1,7 +1,47 @@
-function digitToWord(inputDigit){
-    return "";
+export function digitToWord(inputDigit){
+    switch (inputDigit){
+        case 0:
+            return "Zero";
+        case 1:
+            return "One";
+        case 2:
+            return "Two";
+        case 3:
+            return "Three";
+        case 4:
+            return "Four";
+        case 5:
+            return "Five";
+        case 6:
+            return "Six";
+        case 7:
+            return "Seven";
+        case 8:
+            return "Eight";
+        case 9:
+            return "Nine";
+    }
+    return "ERR";
 }
 
-function numToPhonetics(inputNum){
-    return "";
+export function numToPhonetics(inputNum){
+    if(!Number.isInteger(inputNum)){
+        return "ERR";
+    }
+
+    var phoneticList = [];
+
+    while(inputNum > 0){
+        let digit = inputNum % 10;
+        let phonetic = digitToWord(digit);
+
+        if(phonetic === "ERR"){
+            return "ERR";
+        }
+
+        // Add to beginning of list as we're taking the least significant digit
+        phoneticList.unshift(phonetic);
+        inputNum /= 10;
+    }
+    return phoneticList.length > 0 ? phoneticList.join("") : "ERR";
 }
--- num_phonetics/test/test_num_phonetics.js
+++ num_phonetics/test/test_num_phonetics.js
@@ -1,7 +1,56 @@
-function testDigitToWord(){
+import {digitToWord, numToPhonetics} from "../num_phonetics";
 
+function testDigitToWord(){
+    let testSamples = [
+        [0, "Zero"],
+        [1, "One"],
+        [2, "Two"],
+        [3, "Three"],
+        [4, "Four"],
+        [5, "Five"],
+        [6, "Six"],
+        [7, "Seven"],
+        [8, "Eight"],
+        [9, "Nine"],
+        [13, "ERR"],
+        [1.2, "ERR"],
+        ["str", "ERR"],
+        [{}, "ERR"],
+        [[1,2], "ERR"]
+    ];
+
+    testFunc(testSamples, digitToWord)
 }
 
 function testNumToPhonetics(){
+    let testSamples = [
+        [1234567890, "OneTwoThreeFourFiveSixSevenEightNineZero"],
+        [2424, "TwoFourTwoFour"],
+        [[1,2], "ERR"],
+        [13.2, "ERR"],
+        ["str", "ERR"],
+        [{}, "ERR"]
+    ];
 
+    testFunc(testSamples, numToPhonetics)
 }
+
+function testFunc(list, func){
+    var passed = 0;
+    var failed = 0;
+    for (const v in list) {
+        let ret = func(v[0]);
+        let result = ret === v[1];
+        if(!result){
+            failed++;
+            console.log(`Test failed for ${v[0]} ran on ${func.name}, expected ${v[1]} but got ${ret}`)
+        }else{
+            passed++;
+        }
+    }
+
+    console.log(`${passed + failed} tests ran with ${passed} successes and ${failed} failures.`)
+}
+
+testDigitToWord();
+testNumToPhonetics();
